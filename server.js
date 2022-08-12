e0b42d95845adab15bf741ebc5eb9194351b41fe
Deepti Tiwari <49716268+iDeeptiTiwari@users.noreply.github.com>
2022-08-13 02:11:20 +0530

    Formatted the file

--- server.js
+++ server.js
@@ -1,49 +1,42 @@
-const express = require('express')
-const {exec} = require('child_process')
+const express = require("express");
+const { exec } = require("child_process");
 
-const app = express()
+const app = express();
 
-app.get('/',(req,res) => {
-	
-	res.sendFile(__dirname+"/index.html")
-})
+app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
+app.get("/run", (req, res) => {
+  const cname = req.query.cname;
+  const cimage = req.query.cimage;
+  q = "docker run -itd --name " + cname + " " + cimage;
+  exec(q, (err, stdout, stderr) => {
+    console.log(err);
+    console.log(stderr);
+    res.send("<pre>" + stdout + "</pre>");
+  });
+});
 
-app.get('/run',(req,res)=>{
-	
-    const cname = req.query.cname
-    const cimage = req.query.cimage
-    q = "docker run -itd --name "+cname+" "+cimage
-    exec(q,(err,stdout,stderr)=>{
-        console.log(err)
-        console.log(stderr)
-         res.send("<pre>"+stdout+"</pre>")
-    })
+app.get("/listcontainer", (req, res) => {
+  q = "docker ps";
+  exec(q, (err, stdout, stderr) => {
+    console.log(err);
+    console.log(stderr);
+    res.send("<pre>" + stdout + "</pre>");
+  });
+});
 
-})
-
-app.get('/listcontainer',(req,res)=>{
-
-    q = "docker ps"
-    exec(q,(err,stdout,stderr)=>{
-        console.log(err)
-        console.log(stderr)
-         res.send("<pre>"+stdout+"</pre>")
-    })
-
-})
-
-app.get('/deleteimage',(req,res)=>{
-
-    var iname = req.query.iname
-    q = "sudo docker image rm -f  "+iname
-    exec(q,(err,stdout,stderr)=>{
-        console.log(err)
-        console.log(stderr)
-         res.send("<pre> Deleted "+stdout+"</pre>")
-    })
-
-})
+app.get("/deleteimage", (req, res) => {
+  var iname = req.query.iname;
+  q = "sudo docker image rm -f  " + iname;
+  exec(q, (err, stdout, stderr) => {
+    console.log(err);
+    console.log(stderr);
+    res.send("<pre> Deleted " + stdout + "</pre>");
+  });
+});
 
 app.get("/listimages", (req, res) => {
   q = "docker images";
@@ -54,18 +47,15 @@ app.get("/listimages", (req, res) => {
   });
 });
 
-
-app.get('/deleteimage',(req,res)=>{
-
-    var iname = req.query.iname
-    q = "sudo docker image rm -f  "+iname
-    exec(q,(err,stdout,stderr)=>{
-        console.log(err)
-        console.log(stderr)
-         res.send("<pre> Deleted "+stdout+"</pre>")
-    })
-
-})
+app.get("/deleteimage", (req, res) => {
+  var iname = req.query.iname;
+  q = "sudo docker image rm -f  " + iname;
+  exec(q, (err, stdout, stderr) => {
+    console.log(err);
+    console.log(stderr);
+    res.send("<pre> Deleted " + stdout + "</pre>");
+  });
+});
 
 function emptyExistingDockerFile() {
   command = ": > Dockerfile";
@@ -113,16 +103,16 @@ app.get("/createimage", (req, res) => {
   buildDockerImage(imageName, res);
 });
 
-app.get('/deletecontainer',(req,res)=>{
+app.get("/deletecontainer", (req, res) => {
+  var dname = req.query.dname;
+  q = "sudo docker rm -f " + dname;
+  exec(q, (err, stdout, stderr) => {
+    console.log(err);
+    console.log(stderr);
+    res.send("<pre>" + stdout + "</pre>");
+  });
+});
 
-    var dname = req.query.dname
-    q = "sudo docker rm -f "+dname
-    exec(q,(err,stdout,stderr)=>{
-        console.log(err)
-        console.log(stderr)
-         res.send("<pre>"+stdout+"</pre>")
-    })
-
-})
-
-app.listen(3000,()=>{console.log('web app is running')})
+app.listen(3000, () => {
+  console.log("web app is running");
+});
