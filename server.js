509d1c7bd80ea31f0d616ccee3f06fd5fe0d1c8b
Deepti Tiwari <49716268+iDeeptiTiwari@users.noreply.github.com>
2022-08-12 12:24:58 +0530

    [Deepti | Jay] Add functionality to create image

--- server.js
+++ server.js
@@ -67,6 +67,34 @@ app.get('/deleteimage',(req,res)=>{
 
 })
 
+app.get("/createimage", async (req, res) => {
+  var imageName = req.query.imagename;
+  var fileContent = req.query.dockerfilecontent;
+  console.log("imageName");
+  console.log(imageName);
+  console.log("fileContent");
+  console.log(fileContent);
+
+  q = 'echo "' + fileContent + '" >Dockerfile';
+  console.log(q);
+  await exec(q, (err, stdout, stderr) => {
+    console.log(err);
+    console.log(stderr);
+  });
+  console.log("file is created");
+  q2 = "docker build -t " + imageName + " .";
+  await exec(q2, (err, stdout, stderr) => {
+    console.log(err);
+    console.log(stderr);
+    q3 = "docker images | grep " + imageName;
+    exec(q3, (err, stdout, stderr) => {
+      console.log(err);
+      console.log(stderr);
+      res.send("<pre>" + stdout + "</pre>");
+    });
+  });
+});
+
 app.get('/deletecontainer',(req,res)=>{
 
     var dname = req.query.dname
